5428d580b914b05b1126c29e381746fe5d31b71c
Reggie <bs19293949@outlook.com>
2022-09-25 20:43:53 -0700

    Added Character

---
+++ WV Group 1 Project 1/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float runSpeed;
+    public Rigidbody2D rb;
+
+    private Vector2 moveSpeed;
+    private float move;
+    private float moveHorizontal;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+    }
+
+    void Move()
+    {
+        moveSpeed = new Vector2(moveHorizontal * runSpeed, rb.velocity.y);
+        rb.velocity = moveSpeed;
+    }
+}
